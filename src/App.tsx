38df06c0b229529459fd721dd1a74d38d33f7aba
Dmitry <pravdikov@inbox.ru>
2023-08-17 23:55:16 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from './Todolist';
+
+function App() {
+
+    const tasks = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
+
+    const removeTask = () => {
+        console.log('Hello')
+    }
+
+    return (
+        <div className="App">
+            <Todolist title="What to learn" tasks={tasks} removeTask={removeTask}/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Todolist.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask: ()=> void
+}
+
+export function Todolist(props: PropsType) {
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
+        <ul>
+            {props.tasks.map((el, index)=>{
+                return (
+                    <li>
+                        <input type="checkbox" checked={el.isDone}/>
+                        <span>{el.title}</span>
+                        <button onClick={()=> props.removeTask()} > x </button>
+                    </li>
+                )
+            })}
+        </ul>
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
+}
